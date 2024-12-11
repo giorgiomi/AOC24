ddcc8206b38d4c3e359182dbb3c4bbad1d89ba03
giorgiomi <giorgio.micaglio@studenti.unitn.it>
2024-12-12 00:18:37 +0100

    maybe tomorrow

--- day09/part2.py
+++ day09/part2.py
@@ -8,15 +8,15 @@ def swap(i, j, arr):
 
 def findLast(arr):
     for i, el in reversed(list(enumerate(arr))):
-        if el != '.':
+        if el != -1:
             return i
 
 def findFirst(arr):
     for i, el in enumerate(arr):
-        if el == '.':
+        if el == -1:
             return i
 
-data = str(np.loadtxt('input.txt', dtype=str))
+data = str(np.loadtxt('test.txt', dtype=str))
 blocks = []
 id = 0
 for i, c in enumerate(data):
@@ -24,29 +24,32 @@ for i, c in enumerate(data):
         blocks.extend([id]*int(c))
         id += 1
     else:
-        blocks.extend(['.']*int(c))
+        blocks.extend([-1]*int(c))
 
-# print(blocks)
 #print(len(blocks))
+blocks = np.array(blocks)
+print(blocks)
+#exit()
 
 file_length = 0
 val = blocks[-1]
-for i, el in reversed(list(enumerate(blocks))):
+#for i, el in reversed(list(enumerate(blocks))):
+for i in np.arange(len(blocks) - 1, -1, -1):
     print(i)
-    if el == '.':
+    if blocks[i] == -1:
         continue
     else:
-        if el == blocks[i - 1]:
+        if blocks[i] == blocks[i - 1]:
             file_length += 1
             continue
         else:
             file_length += 1
             # insert cycle here
-            for l in range(len(blocks)):
+            for l in np.arange(len(blocks)):
                 i_first = l + findFirst(blocks[l:])
                 dot_length = 0
                 for j, el2 in list(enumerate(blocks))[i_first:]:
-                    if el2 == '.':
+                    if el2 == -1:
                         dot_length += 1
                     else:
                         break
@@ -54,7 +57,7 @@ for i, el in reversed(list(enumerate(blocks))):
                 # print(f"file: {file_length}")
                 if dot_length >= file_length and i > i_first:
                     # do the swap
-                    print(f"swapping file of {file_length} in {dot_length} dots")
+                    #print(f"swapping file of {file_length} in {dot_length} dots")
                     for k in range(file_length):
                         blocks = swap(i + k, i_first + k, blocks)
                         #print("swapping")
@@ -69,6 +72,6 @@ print(blocks)
 
 checksum = 0
 for i, el in enumerate(blocks):
-    if el != '.':
+    if el != -1:
         checksum += el*i
 print(checksum)
