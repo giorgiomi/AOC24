e93557aa7cd0f7eb65636aa5cd1569ef9a53852e
giorgiomi <giorgio.micaglio@studenti.unitn.it>
2024-12-12 17:06:21 +0100

    reorder day09

--- day09/part2.py
+++ day09/part2.py
@@ -1,77 +1,49 @@
 import numpy as np
+import sys
 
-def swap(i, j, arr):
-    t = arr[i]
-    arr[i] = arr[j]
-    arr[j] = t
-    return arr
+file = sys.argv[1]
 
-def findLast(arr):
-    for i, el in reversed(list(enumerate(arr))):
-        if el != -1:
-            return i
+with open(f'{file}.txt', 'r') as f:
+    data = np.array([int(x) for x in f.read()])
 
-def findFirst(arr):
-    for i, el in enumerate(arr):
-        if el == -1:
-            return i
+    ids = np.arange(int(len(data)/2) + 1)
 
-data = str(np.loadtxt('test.txt', dtype=str))
-blocks = []
-id = 0
-for i, c in enumerate(data):
-    if i % 2 == 0:
-        blocks.extend([id]*int(c))
-        id += 1
-    else:
-        blocks.extend([-1]*int(c))
+    lengths = data[::2]
+    dots = data[1::2]
+    files = np.array([(lengths[i], ids[i]) for i in range(len(lengths))])
 
-#print(len(blocks))
-blocks = np.array(blocks)
-print(blocks)
-#exit()
+    i = len(files) - 1
+    while (i >= 0):
+        file_len = files[i][0]
+        file_id = files[i][1]
+        for j in np.arange(len(dots)):
+            if j < i:
+                dot_len = dots[j]
+                if dot_len >= file_len:
+                    #swap
+                    files = np.delete(files, i, axis=0)
+                    files = np.insert(files, j+1, (file_len, file_id), axis=0)
 
-file_length = 0
-val = blocks[-1]
-#for i, el in reversed(list(enumerate(blocks))):
-for i in np.arange(len(blocks) - 1, -1, -1):
-    print(i)
-    if blocks[i] == -1:
-        continue
-    else:
-        if blocks[i] == blocks[i - 1]:
-            file_length += 1
-            continue
-        else:
-            file_length += 1
-            # insert cycle here
-            for l in np.arange(len(blocks)):
-                i_first = l + findFirst(blocks[l:])
-                dot_length = 0
-                for j, el2 in list(enumerate(blocks))[i_first:]:
-                    if el2 == -1:
-                        dot_length += 1
+                    dots = np.insert(dots, j, 0)
+                    dots[j + 1] -= file_len
+                    if i == len(files) - 1:
+                        dots = np.delete(dots, -1)
                     else:
-                        break
-                # print(f"dots: {dot_length}")
-                # print(f"file: {file_length}")
-                if dot_length >= file_length and i > i_first:
-                    # do the swap
-                    #print(f"swapping file of {file_length} in {dot_length} dots")
-                    for k in range(file_length):
-                        blocks = swap(i + k, i_first + k, blocks)
-                        #print("swapping")
-                    #print(blocks)
+                        dots[i] += dots[i+1] + file_len
+                        dots = np.delete(dots, i+1)
+                    
+                    i += 1
                     break
-                else:
-                    # move to next dots
-                    continue
-            file_length = 0
-    
-print(blocks)
+        i -= 1
 
-checksum = 0
-for i, el in enumerate(blocks):
-    if el != -1:
-        checksum += el*i
-print(checksum)
+    pos = 0
+    checksum = 0
+    for i, f in enumerate(files):
+        for k in range(f[0]):
+            checksum += f[1] * (pos + k)
+        pos += f[0]
+        if i == len(files) - 1:
+            break
+        else:
+            pos += dots[i]
+    print(checksum)
