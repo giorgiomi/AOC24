04ef691918aa137e6e7c376ae3ae296b6584c70e
giorgiomi <giorgio.micaglio@studenti.unitn.it>
2024-12-12 17:47:31 +0100

    day06 BF (stands for Best Friend ofc)

--- day06/part2B.py
+++ day06/part2B.py
@@ -1,4 +1,5 @@
 import numpy as np
+from copy import copy, deepcopy
 
 def findGuard(data):
     for i, a in enumerate(data):
@@ -8,99 +9,83 @@ def findGuard(data):
 
 with open('input.txt', 'r') as f:
     data = [[el for el in line if el != '\n'] for line in f]
+    map = deepcopy(data)
+    # print(np.matrix(map))
 
     # initial position and direction
-    i, j = findGuard(data)
-    data[i][j] = 'N'
+    i_start, j_start = findGuard(data)
+    data[i_start][j_start] = 'N'
     dir = 'N'
+    i, j = i_start, j_start
 
     # directions list
     directions = ['N', 'E', 'S', 'W']
 
     count = 0
-    while (True):
+    for k in range(len(map)):
+        for l in range(len(map[k])):
+            if map[k][l] == '.':
+                data = deepcopy(map)
+                data[i_start][j_start] = 'N'
+                dir = 'N'
+                i, j = i_start, j_start
 
-        if dir == 'N':
-            if i == 0: break
-            else:
-                look = data[i-1][j]
-                if look == '.' or look in directions:
-                    # if look == 'E':
-                    #     count += 1
-                    #     print(np.matrix(data))
-                    # else:
-                    for j_scan in range(j + 1, len(data[i])):
-                        if data[i][j_scan] == '#': break
-                        if data[i][j_scan] == 'E':
-                            count += 1
-                            # print(np.matrix(data))
-                            break
-                    data[i-1][j] = 'N'
-                    i -= 1
-                elif look == '#': dir = 'E'
+                print(f"inserting # in {k, l}")
+                data[k][l] = '#'
+                # print(np.matrix(data))
+
+                # solve labirinth
+                while (True):
+                    if dir == 'N':
+                        if i == 0: break
+                        else:
+                            look = data[i-1][j]
+                            if look == '.' or look in directions:
+                                if look == dir:
+                                    count += 1
+                                    break
+                                data[i-1][j] = 'N'
+                                i -= 1
+                            elif look == '#': dir = 'E'
+                            
+                    if dir == 'E':
+                        if j == len(data[i]) - 1: break
+                        else:
+                            look = data[i][j+1]
+                            if look == '.' or look in directions:
+                                if look == dir:
+                                    count += 1
+                                    break
+                                data[i][j+1] = 'E'
+                                j += 1
+                            elif look == '#': dir = 'S'
+                            
+                    if dir == 'S':
+                        if i == len(data) - 1: break
+                        else:
+                            look = data[i+1][j]
+                            if look == '.' or look in directions:
+                                if look == dir:
+                                    count += 1
+                                    break
+                                data[i+1][j] = 'S'
+                                i += 1
+                            elif look == '#': dir = 'W'
+                            
+                    if dir == 'W':
+                        if j == 0: break
+                        else:
+                            look = data[i][j-1]
+                            if look == '.' or look in directions:
+                                if look == dir:
+                                    count += 1
+                                    break
+                                data[i][j-1] = 'W'
+                                j -= 1
+                            elif look == '#': dir = 'N'
+
+                # print(np.matrix(data))
+                # data[k][l] = '.'
                 
 
-        if dir == 'E':
-            if j == len(data[i]) - 1: break
-            else:
-                look = data[i][j+1]
-                if look == '.' or look in directions:
-                    # if look == 'S':
-                    #     count += 1
-                    #     print(np.matrix(data))
-                    # else:
-                    for i_scan in range(i + 1, len(data)):
-                        if data[i_scan][j] == '#': break
-                        if data[i_scan][j] == 'S':
-                            count += 1
-                            # print(np.matrix(data))
-                            break
-                    data[i][j+1] = 'E'
-                    j += 1
-                elif look == '#': dir = 'S'
-                
-
-        if dir == 'S':
-            if i == len(data) - 1: break
-            else:
-                look = data[i+1][j]
-                if look == '.' or look in directions:
-                    # if look == 'W':
-                    #     count += 1
-                    #     print(np.matrix(data))
-                    # else:
-                    for j_scan in range(j - 1, -1, -1):
-                        if data[i][j_scan] == '#': break
-                        if data[i][j_scan] == 'W':
-                            count += 1
-                            # print(np.matrix(data))
-                            break
-                    data[i+1][j] = 'S'
-                    i += 1
-                elif look == '#': dir = 'W'
-                
-
-        if dir == 'W':
-            if j == 0: break
-            else:
-                look = data[i][j-1]
-                if look == '.' or look in directions:
-                    # if look == 'N':
-                    #     count += 1
-                    #     print(np.matrix(data))
-                    # else:
-                    for i_scan in range(i - 1, -1, -1):
-                        if data[i_scan][j] == '#': break
-                        if data[i_scan][j] == 'N':
-                            count += 1
-                            # print(np.matrix(data))
-                            break
-                    data[i][j-1] = 'W'
-                    j -= 1
-                elif look == '#': dir = 'N'
-                
-
-#print(np.matrix(data))
 print(count)
-
-#print(list(range(5, -1, -1)))
